947521325f97f9081fcad58d62d41e134a1a26c5
HrechykhaYurii <grechikha.yura@gmail.com>
2023-03-21 21:45:36 +0200

    add fetch

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import { createPortal } from "react-dom";
+import { Component } from "react";
+import PropTypes from 'prop-types';
+import { Overlay, ModalViewer, ModalImg } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.closeByEsc);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.closeByEsc);
+  }
+
+  closeByEsc = e => {
+    if (e.code !== 'Escape') {
+      return;
+    }
+    this.props.closeModal();
+  };
+
+  render() {
+    const { modalImg,tags, closeModal } = this.props;
+
+    return createPortal(<Overlay onClick={closeModal}>
+        <ModalViewer>
+          <ModalImg src={modalImg} alt={tags} />
+        </ModalViewer>
+      </Overlay>,modalRoot)
+  }
+}
+
+Modal.propTypes = {
+  modalImg: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  tags: PropTypes.string.isRequired,
+};
